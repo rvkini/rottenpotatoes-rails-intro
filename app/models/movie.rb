984fec6efa5618da5f0a6c3926266a8cb7d00b9e
rvkini <55466082+rvkini@users.noreply.github.com>
2019-10-01 12:43:44 -0500

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ActiveRecord::Base
-    class Movie < ActiveRecord::Base
+        @@all_ratings
         def self.all_ratings
-            self.find(:all, :select => "rating", :group => "rating").map(&:rating)
+            #self.find(:all, :select => "rating", :group => "rating").map(&:rating)
+            @@all_ratings = ['G', 'PG', 'PG-13', 'R']
         end
-    end
+        def self.populated
+            return {'G'=>'1', 'PG'=>'1', 'PG-13'=>'1', 'R'=>'1'}
+        end
 end
