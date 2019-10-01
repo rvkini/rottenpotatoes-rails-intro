9e5a8a5510455fa7c454e7048d6b1b04e7efaf11
rvkini <55466082+rvkini@users.noreply.github.com>
2019-09-30 22:00:46 -0500

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    class Movie < ActiveRecord::Base
+        def self.all_ratings
+            self.find(:all, :select => "rating", :group => "rating").map(&:rating)
+        end
+    end
 end
